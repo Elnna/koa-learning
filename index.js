7fa63be1305b3ffb0db3a7300027f84a9e5cbe44
Elnna <grahamhuang@126.com>
2020-09-21 22:09:12 +0800

    next chapter content

--- index.js
+++ index.js
@@ -1,6 +1,61 @@
 const Koa = require('koa');
+const Router =  require('koa-router');
+const Cors = require('@koa/cors');
+const koaBody = require('koa-body');
+const json = require('koa-json');
+
 const app = new Koa();
-app.use( async ctx =>{
-    ctx.body = 'hello world, this is a test';
+const router = new Router();
+const cors = new Cors();
+
+router.prefix('/api');
+router.get('/',ctx =>{
+    console.log(ctx);
+    console.log(ctx.request);
+    ctx.body = 'hello localhost';
 });
+
+router.get('/api',ctx =>{
+    //get params
+    const params = ctx.request.query
+    console.log(params);
+    // console.log(ctx);
+    // console.log(ctx.request);
+    console.log(params.name,params.age);
+    ctx.body = {
+        "name":params.name,
+        "age":params.age
+    };
+})
+router.get('/async', async (ctx) =>{
+   let result =  await new Promise((resolve) =>{
+       setTimeout(function (){
+           resolve('Hello world 2s later');
+       },3000)
+    });
+   ctx.body = result;
+
+})
+router.post('/post',async(ctx) =>{
+    let{ body } =  ctx.request;
+    console.log(body);
+    console.log(ctx.request);
+    ctx.body = {
+        ...body
+    }
+    // console.log(ctx.body);
+})
+app.use(koaBody())
+    .use(Cors())
+    .use(json({pretty:false,param:'pretty'}));
+
+app.use(router.routes())
+    .use(router.allowedMethods());
+
+
+//1.request,method, respond
+//2.api url => function ,router
+//3.ctx,async,
+
+
 app.listen(3000);
--- learning-progress.md
+++ learning-progress.md
@@ -1,4 +1,25 @@
 #learning progress
+#Chpater One
 1.what is koa
 original site :https://koajs.com
 Chinese site : https://koa.bootcss.com
+简洁快速的使用node搭建web框架
+
+2.router
+npm install -S koa-router
+
+3.middleware 中间件 丰富
+
+4.async await
+#Chpter Two Koa 开发RESTful接口
+1.kow-router
+
+2.协议解析：koa-body
+
+3.跨域处理：@koa/cors
+
+4.数据处理：koa-json
+stringfy
+
+#Chapter Three 开发目录结构
+
